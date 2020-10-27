82f20bc90cd9d090b3415b8ad40abcc93c22abf4
AlexGralewski <gralewski.aleksander@gmail.com>
2020-10-27 12:46:01 +0100

    Updates

---
+++ precache-manifest.5d34052de688627452065744f8bc460a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "924d021fcf878b49a6c15ae1e911b57a",
+    "url": "/weather-app/index.html"
+  },
+  {
+    "revision": "42f7a766880482ae6a34",
+    "url": "/weather-app/static/css/main.874fa720.chunk.css"
+  },
+  {
+    "revision": "70b339c084d2eb7ce3cf",
+    "url": "/weather-app/static/js/2.60b2a386.chunk.js"
+  },
+  {
+    "revision": "f5fc2afae58c4ffc30c6148864765d4b",
+    "url": "/weather-app/static/js/2.60b2a386.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "42f7a766880482ae6a34",
+    "url": "/weather-app/static/js/main.df2f8ba5.chunk.js"
+  },
+  {
+    "revision": "d0e2e579b5155f255804",
+    "url": "/weather-app/static/js/runtime-main.e6f382d7.js"
+  }
+]);
