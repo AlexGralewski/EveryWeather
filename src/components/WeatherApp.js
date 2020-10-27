40f5e67d65ce02b65b3545805eaa5035646b23c5
AlexGralewski <gralewski.aleksander@gmail.com>
2020-10-27 10:00:04 +0100

    Added components and css folders

---
+++ src/components/WeatherApp.js
@@ -0,0 +1,20 @@
+import React from "react"
+
+class WeatherApp extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+    
+    }
+  }
+
+  render() {
+    return (
+      <div className="weather-app">
+         
+      </div>
+    )
+  }
+}
+
+export default WeatherApp
