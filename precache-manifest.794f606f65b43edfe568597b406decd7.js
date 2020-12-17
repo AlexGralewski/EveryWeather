cb7ee2e14e4bfd0378309ff9ec3057c6472d412c
AlexGralewski <gralewski.aleksander@gmail.com>
2020-12-17 12:44:40 +0100

    Updates

---
+++ precache-manifest.794f606f65b43edfe568597b406decd7.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f96b46307c04c95d8c391af96159775d",
+    "url": "/weather-app/index.html"
+  },
+  {
+    "revision": "06758a09aae3a9b8b5a5",
+    "url": "/weather-app/static/css/main.e837972a.chunk.css"
+  },
+  {
+    "revision": "c20ff60a1f0515e53a9d",
+    "url": "/weather-app/static/js/2.6acc6498.chunk.js"
+  },
+  {
+    "revision": "f5fc2afae58c4ffc30c6148864765d4b",
+    "url": "/weather-app/static/js/2.6acc6498.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "06758a09aae3a9b8b5a5",
+    "url": "/weather-app/static/js/main.444aefb1.chunk.js"
+  },
+  {
+    "revision": "d0e2e579b5155f255804",
+    "url": "/weather-app/static/js/runtime-main.e6f382d7.js"
+  },
+  {
+    "revision": "e6e20999a0d9c24cb13114968b5b5545",
+    "url": "/weather-app/static/media/clear-day-bg.e6e20999.jpg"
+  },
+  {
+    "revision": "41a70beb9225ac88a47b21c5ceeeb21d",
+    "url": "/weather-app/static/media/clear-night-bg.41a70beb.jpg"
+  },
+  {
+    "revision": "f3e046616b6ac3711f0e6de470cdb98c",
+    "url": "/weather-app/static/media/clouds-bg.f3e04661.jpg"
+  },
+  {
+    "revision": "c7da77ed36b51093ffa0c7b9fb5ae9d7",
+    "url": "/weather-app/static/media/clouds-day-bg.c7da77ed.jpg"
+  },
+  {
+    "revision": "5082da6bcbb67d4004f8cefcd141148a",
+    "url": "/weather-app/static/media/clouds-night-bg.5082da6b.jpg"
+  },
+  {
+    "revision": "0e4c237b648180f8475b2c509b534458",
+    "url": "/weather-app/static/media/rain-bg.0e4c237b.jpg"
+  },
+  {
+    "revision": "6d903157b1e1a50fc43e17f1f0c21abc",
+    "url": "/weather-app/static/media/rain-day-bg.6d903157.jpg"
+  },
+  {
+    "revision": "e42717272d2fdbe18b3be25bda4e4deb",
+    "url": "/weather-app/static/media/rain-night-bg.e4271727.jpg"
+  },
+  {
+    "revision": "b69e0130cad34bd043562c21a6e0cc0b",
+    "url": "/weather-app/static/media/smog-bg.b69e0130.jpg"
+  },
+  {
+    "revision": "8b7e32f5dea522a5cb7d7e51dbcf11b0",
+    "url": "/weather-app/static/media/snow-bg.8b7e32f5.jpg"
+  },
+  {
+    "revision": "a2db74f2036f44a0845529e166154c9c",
+    "url": "/weather-app/static/media/starting-bg.a2db74f2.jpg"
+  },
+  {
+    "revision": "7a3cd65118232efafc87ae28fb53ac8b",
+    "url": "/weather-app/static/media/storm-bg.7a3cd651.png"
+  }
+]);
