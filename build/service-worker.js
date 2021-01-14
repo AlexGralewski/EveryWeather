443180672dd5fbe3c756e0b0b6ac634a957282b9
AlexGralewski <gralewski.aleksander@gmail.com>
2021-01-14 17:15:04 +0100

    Aesthetics improvements

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/weather-app/precache-manifest.089b9d489e4122b3cd4098cc0ffd1fe6.js"
+  "/weather-app/precache-manifest.77aaf42fca273e02bb74182f52c6b3e6.js"
 );
 
 self.addEventListener('message', (event) => {
