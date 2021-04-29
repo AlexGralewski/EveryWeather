b3737c0d777faac6a745373c0ab5dd9acd3afdc0
AlexGralewski <gralewski.aleksander@gmail.com>
2021-04-29 09:07:36 +0200

    Changed socials links URLs

--- src/components/Socials.js
+++ src/components/Socials.js
@@ -3,11 +3,11 @@ import React from "react"
 export default function Socials() {
   return(
     <div className="socials">
-      <a href="https://www.linkedin.com/in/aleksander-gralewski-1a01a7185/">
+      <a href="https://www.linkedin.com/in/aleksandergralewski/">
 
       <div className="social linkedin"><i class="fab fa-linkedin-in"></i></div>
       </a>
-      <a href="https://www.linkedin.com/in/aleksander-gralewski-1a01a7185/">
+      <a href="https://github.com/AlexGralewski">
 
       <div className="social github"><i class="fab fa-github"></i></div>
       </a>
